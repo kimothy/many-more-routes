d0059dffd11a51075ce630a2ce0d665016fa726b
Kim TImothy Engh <kimothy@users.noreply.github.com>
2022-04-29 00:08:22 +0200

    new next() and generator() func with validation

--- src/sequence.py
+++ src/sequence.py
@@ -1,22 +1,7 @@
-from typing import Optional
+from typing import Optional, List
 
-def generator(sequenceNumber: Optional[str] = None, n: Optional[int] = None):
-    '''Returns a sequence generator. If n is not given it will loop infinitely'''
-    if n:
-        for _ in range(n):
-            sequenceNumber = next(sequenceNumber)
-            yield sequenceNumber
-
-    else:
-        while True:
-            sequenceNumber = next(sequenceNumber)
-            yield sequenceNumber
-        
-
-
-def next(sequenceNumber: Optional[str] = None):
+def next(sequenceNumber: Optional[str] = None) -> str:
     '''Taked a route sequence number and incriments by one.
-
     >>> next(None)
     'AA0001'
     >>> next('AA0001')
@@ -27,11 +12,50 @@ def next(sequenceNumber: Optional[str] = None):
     'AA0001'
     '''
     if sequenceNumber:
+        _validate_seed(sequenceNumber)
+
+    return _next(sequenceNumber)
+
+
+def generator(sequenceNumber: Optional[str] = None, n: Optional[int] = None) -> List[str]:
+    '''Returns a sequence generator. If n is not given it will loop infinitely'''
+    if sequenceNumber:
+        _validate_seed(sequenceNumber)
+
+    return _generator(sequenceNumber, n)
+
+
+def _validate_seed(sequenceNumber: str) -> None:
+    '''Validate the input of a seed'''
+    if len(sequenceNumber) != 6:
+        raise ValueError('The sequence number has to be exatly 6 chars long')
+    elif not sequenceNumber[:2].isalpha():
+        raise ValueError('The first to chars of the sequence number has to be alpha numeric')
+    elif not sequenceNumber[2:].isdigit():
+        raise ValueError('The last four chars of the sequence number has to be numeric')
+
+def _generator(sequenceNumber: Optional[str] = None, n: Optional[int] = None) -> List[str]:
+    '''Returns a sequence generator. If n is not given it will loop infinitely'''
+    if n:
+        for _ in range(n):
+            sequenceNumber = _next(sequenceNumber)
+            yield sequenceNumber
+
+    else:
+        while True:
+            sequenceNumber = _next(sequenceNumber)
+            yield sequenceNumber
+        
+
+
+def _next(sequenceNumber: Optional[str] = None) -> str:
+    '''Taked a route sequence number and incriments by one.'''
+    if sequenceNumber:
         alpha = sequenceNumber[:2]
         digit = sequenceNumber[2:]
 
-        newDigit = incrementDigit(digit)
-        newAlpha = incrementAlpha(alpha) if newDigit == next()[2:] else alpha
+        newDigit = _incrementDigit(digit)
+        newAlpha = _incrementAlpha(alpha) if newDigit == next()[2:] else alpha
 
         return newAlpha + newDigit
 
@@ -39,7 +63,7 @@ def next(sequenceNumber: Optional[str] = None):
         return 'AA0001'
 
         
-def incrementAlpha(alpha: str) -> str:
+def _incrementAlpha(alpha: str) -> str:
     '''Takes a upper char string and increments as it's a number sequence.
     
     >>> incrementAlpha('AA')
@@ -62,7 +86,7 @@ def incrementAlpha(alpha: str) -> str:
 
 
 
-def incrementDigit(alpha: str) -> str:
+def _incrementDigit(alpha: str) -> str:
     ''' Takes a digit as string type and returns as equal length number as string.
         Number is reset to 1 when max number is reached.    
      '''
